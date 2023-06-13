abb23a4203968a0cd654ef50d695cdba7c32e2cc
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2023-06-13 14:49:36 +0900

    Layout container, columns, display, block, float, clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='text-white bg-slate-900 h-screen'>
-      <div className='container mx-auto'>
-        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4 '>
-          <div className='p-6 rounded-lg bg-sky-500'>1</div>
-          <div className='p-6 rounded-lg bg-sky-500'>2</div>
-          <div className='p-6 rounded-lg bg-sky-500'>3</div>
-          <div className='p-6 rounded-lg bg-red-500 col-span-2'>4</div>
-          <div className='p-6 rounded-lg bg-sky-500'>5</div>
-          <div className='p-6 rounded-lg bg-sky-500'>6</div>
-          <div className='p-6 rounded-lg bg-violet-500 col-start-3'>7</div>
-          <div className='p-6 rounded-lg bg-violet-500 col-start-1 col-end-3'>
-            8
-          </div>
-        </div>
-        {/* Auto-cols Using like flex  */}
-        <div className='grid grid-flow-col auto-cols-max'>
-          <div className='p-6 rounded-lg bg-sky-500'>1</div>
-          <div className='p-6 rounded-lg bg-sky-500'>2</div>
-          <div className='p-6 rounded-lg bg-sky-500'>3</div>
-        </div>
-        {/* row-span */}
-        <div className='grid gap-4 grid-flow-col grid-rows-4 '>
-          <div className='p-6 rounded-lg bg-yellow-500 row-span-2'>1</div>
-          <div className='p-6 rounded-lg bg-yellow-500'>2</div>
-          <div className='p-6 rounded-lg bg-yellow-500'>3</div>
-          <div className='p-6 rounded-lg bg-yellow-500 row-span-2'>4</div>
-          <div className='p-6 rounded-lg bg-yellow-500 row-span-3'>5</div>
-          <div className='p-6 rounded-lg bg-yellow-500 row-start-2'>6</div>
-          <div className='p-6 rounded-lg bg-yellow-500'>7</div>
-        </div>
-        {/* grid-flow-row-dense reduce empty space */}
-        <div className='grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-3'>
-          <div className='col-span-2 p-6 rounded-lg bg-cyan-500'>1</div>
-          <div className='p-6 rounded-lg bg-cyan-500'>2</div>
-          <div className='p-6 rounded-lg bg-cyan-500'>4</div>
-          <div className='col-span-2 p-6 rounded-lg bg-cyan-500'>5</div>
-          <div className='p-6 rounded-lg bg-cyan-500'>6</div>
-        </div>
+    <main className='h-screen text-white  Parent bg-slate-900'>
+      {/* layout container */}
+      <div className='container px-2 bg-red-500'>
+        <h1>Hello</h1>
+      </div>
+      {/* layout columns */}
+      {/* <div className='container px-2 columns-3'> */}
+      <div className='container px-2 '>
+        <img
+          // layout display
+          // className='block'
+          // layout float
+          className='w-6/12 float-right'
+          src='https://media.istockphoto.com/id/1401461127/ko/%EC%82%AC%EC%A7%84/%EC%95%8C%EB%A6%BC-%EC%95%8C%EB%A6%BC%EC%9D%B4%EC%9E%88%EB%8A%94-%EC%A0%84%EC%9E%90-%EB%A9%94%EC%9D%BC%EC%97%90-%EC%8A%A4%EB%A7%88%ED%8A%B8-%ED%8F%B0%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EC%82%AC%EC%97%85%EA%B0%80%EC%9D%98-%EC%86%90.jpg?s=2048x2048&w=is&k=20&c=uNMOAErvp3Wc_5WvqQmaoIiYbWMmypTAziZ057o9SBY='
+          alt='No Image'
+        ></img>
+        <img
+          className='w-2/6 float-left'
+          src='https://media.istockphoto.com/id/1401461127/ko/%EC%82%AC%EC%A7%84/%EC%95%8C%EB%A6%BC-%EC%95%8C%EB%A6%BC%EC%9D%B4%EC%9E%88%EB%8A%94-%EC%A0%84%EC%9E%90-%EB%A9%94%EC%9D%BC%EC%97%90-%EC%8A%A4%EB%A7%88%ED%8A%B8-%ED%8F%B0%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EC%82%AC%EC%97%85%EA%B0%80%EC%9D%98-%EC%86%90.jpg?s=2048x2048&w=is&k=20&c=uNMOAErvp3Wc_5WvqQmaoIiYbWMmypTAziZ057o9SBY='
+          alt='No Image'
+        ></img>
+        {/* layout clear */}
+        <h1 className='clear-left'>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </h1>
+        <h1>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </h1>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    // same as mx-auto option
+    container: {
+      center: true,
+    },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     spacing: {
-      '1': '100px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px'
+      1: "100px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        customGreen:{
-          s1:'#370699',
-          s2:'#99068d'
-        }
+      colors: {
+        customGreen: {
+          s1: "#370699",
+          s2: "#99068d",
+        },
       },
     },
   },
   plugins: [],
-}
+};
